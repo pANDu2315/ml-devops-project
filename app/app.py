f194416ee6053009d1976e64edc895a12e0fabb8
pANDu2315 <ueuauk4ever@gmail.com>
2026-01-06 07:05:26 +0530

    Complete DevOps pipeline demo

---
+++ app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+from prometheus_client import Counter, generate_latest
+
+app = Flask(__name__)
+REQUEST_COUNT = Counter("request_count", "Total HTTP Requests")
+
+@app.route("/")
+def home():
+    REQUEST_COUNT.inc()
+    return jsonify(message="Flask DevOps App Running")
+
+@app.route("/predict")
+def predict():
+    REQUEST_COUNT.inc()
+    return jsonify(prediction=5)
+
+@app.route("/metrics")
+def metrics():
+    return generate_latest()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
